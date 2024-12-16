6434d7ca0a99be5962972a7b55e200a21ac1ce14
Natan Cunha <104222003+natancunhadev@users.noreply.github.com>
2024-12-16 12:35:32 -0800

    Add files via upload

---
+++ teste variavel string.py
@@ -0,0 +1,16 @@
+#string: str 
+
+
+nome = "Natan"
+sobrenome = 'Cunha'
+print(nome, type(nome))
+print(sobrenome, type(sobrenome))
+
+nome_completo = nome + sobrenome 
+print(nome_completo)
+
+
+nome_completo2 = nome + " " + sobrenome
+print(nome_completo2)
+
+#variavelstring se concatena apenas numero com numero. 
